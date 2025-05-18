59cc21cd6a804ccc12acf1e7706c640b6400040d
hindipower <omwneon@gmail.com>
2025-05-19 00:07:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
+from flask import Flask, render_template, request, send_file
+import yt_dlp
+import os
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.json.get('url')
+    output_path = "downloads/%(title)s.%(ext)s"
+    
+    if not os.path.exists("downloads"):
+        os.makedirs("downloads")
+
+    ydl_opts = {
+        'outtmpl': output_path,
+        'format': 'best',
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url)
+        filename = ydl.prepare_filename(info)
+
+    return send_file(filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
